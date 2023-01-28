199c8ca0c87e4c5ff5b511939ae21c0af3ddbc65
Peklerfix <peklerfix@gmail.com>
2023-01-28 10:49:45 +0100

    initial commit

---
+++ Intersys/Program.cs
@@ -0,0 +1,28 @@
+﻿//Reverse Array
+void PrintReverseArray()
+{
+	int arraySize;
+	bool setArraySizeSuccess = false;
+	string[] inputsArray;
+
+	while (!setArraySizeSuccess)
+	{
+		Console.WriteLine("Please set array size (size must be a number):");
+		var getArraySizeFromUser = Console.ReadLine();
+
+		if (!int.TryParse(getArraySizeFromUser, out arraySize))
+		{
+			Console.WriteLine("Array size must be specified as a number");
+			continue;
+		}
+		Console.WriteLine($"Array size is {arraySize}");
+		setArraySizeSuccess = true;
+		inputsArray = new string[arraySize];
+	}
+
+
+
+
+
+}
+PrintReverseArray();
