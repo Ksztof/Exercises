17b1a5b988b441fc4f9d5ea654d7a4b7a11f2bee
Peklerfix <peklerfix@gmail.com>
2023-01-29 13:28:32 +0100

    .NET Core web app has been created with draft of OptionalPow function

---
+++ IntersysJs/wwwroot/js/Index.js
@@ -0,0 +1,18 @@
+﻿
+
+
+
+function createButton() {
+    var btn = document.createElement("button");
+    btn.innerHTML = "Optional Power";
+    btn.addEventListener("click", function () {
+        var num1 = parseFloat(prompt("Give me number1:"));
+        var num2 = parseFloat(prompt("Give me number2: "));
+    });
+    document.body.appendChild(btn);
+}
+
+
+function optionalPow(a, b) {
+    
+}
