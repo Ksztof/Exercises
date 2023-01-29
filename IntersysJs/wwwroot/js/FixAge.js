43c79978545b3ad2f311311a58bb054082f91921
Peklerfix <peklerfix@gmail.com>
2023-01-29 16:58:05 +0100

    Validation in FixAge.js for entered number has been changed from || to && also I don't use var, instead I used let

---
+++ IntersysJs/wwwroot/js/FixAge.js
@@ -0,0 +1,40 @@
+﻿function createButton2()
+{
+    let btn = document.createElement("button");
+    btn.innerHTML = "Fix Age";
+    btn.addEventListener("click", function ()
+    {
+        let userInput = prompt("Pleas give me array of integers, separated by commas:");
+        let userInputArray = userInput.split(",").map(function (item) {
+            return parseFloat(item);
+        });
+        let result = fixage(userInputArray);
+        alert(result);
+    });
+
+    document.body.appendChild(btn);
+}
+
+function fixage(arrayOfIntegers)
+{
+    let wantedAgeArray = [];
+    if (!Array.isArray(arrayOfIntegers)) {
+        return 'NA';
+    }
+
+    arrayOfIntegers.forEach(number =>
+    {
+        if (number >= 18 && number <= 60)
+        {
+            wantedAgeArray.push(number);
+        }
+       
+    });
+    if (wantedAgeArray.length === 0) {
+        return "NA";
+    }
+    else
+    {
+        return wantedAgeArray;
+	}
+}
---
+++ IntersysJs/wwwroot/js/OptionalPower.js
@@ -0,0 +1,42 @@
+﻿function createButton()
+{
+    let btn = document.createElement("button");
+    btn.innerHTML = "Optional Power";
+    btn.addEventListener("click", function () {
+        let num1 = parseFloat(prompt("Give me number1:"));
+        let num2 = parseFloat(prompt("Give me number2: "));
+        let result = askUser();
+        if (result) {
+           let powResult = optionalPow(num1, num2)
+        }
+        else
+        {
+            let powResult = optionalPow(num2, num1)
+        }
+
+        alert(`${powResult}`)
+
+    });
+
+    document.body.appendChild(btn);
+
+}
+
+
+function askUser()
+{
+    let question = confirm("Do you like like programming");
+    if (question)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+	}
+}
+
+
+function optionalPow(num1, num2) {
+    return Math.pow(num1, num2)
+}
