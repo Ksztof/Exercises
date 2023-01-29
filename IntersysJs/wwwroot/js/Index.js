85935d6d6c9205f0869d6061257117b36cc95cd0
Peklerfix <peklerfix@gmail.com>
2023-01-29 14:20:11 +0100

    Exercise Optional Power is done

--- IntersysJs/wwwroot/js/Index.js
+++ IntersysJs/wwwroot/js/Index.js
@@ -1,18 +1,42 @@
-﻿
-
-
-
-function createButton() {
+﻿function createButton()
+{
     var btn = document.createElement("button");
     btn.innerHTML = "Optional Power";
     btn.addEventListener("click", function () {
         var num1 = parseFloat(prompt("Give me number1:"));
         var num2 = parseFloat(prompt("Give me number2: "));
+        var result = askUser();
+        if (result) {
+           var powResult = optionalPow(num1, num2)
+        }
+        else
+        {
+            var powResult = optionalPow(num2, num1)
+        }
+
+        alert(`${powResult}`)
+
     });
+
     document.body.appendChild(btn);
+
 }
 
 
-function optionalPow(a, b) {
-    
+function askUser()
+{
+    var question = confirm("Do you like like programming");
+    if (question)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+	}
+}
+
+
+function optionalPow(num1, num2) {
+    return Math.pow(num1, num2)
 }
