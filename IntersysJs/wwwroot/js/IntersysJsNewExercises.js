6fc7682f6e01a23d07d6ce6c4742d1e1d60e0ca8
Peklerfix <peklerfix@gmail.com>
2023-02-04 12:40:47 +0100

    function alertyourInput has been created user can alert entered text

---
+++ IntersysJs/wwwroot/js/IntersysJsNewExercises.js
@@ -0,0 +1,27 @@
+﻿function alertYourInput() {
+	let button = document.createElement("button");
+	button.innerHTML = "alert your input";
+	button.style.color = "black";
+	button.style.backgroundColor = "white";
+
+	button.addEventListener("click", function () {
+		let form = document.createElement("form");
+		form.id = "alertFormInput";
+		let input = document.createElement("input");
+		let submitButton = document.createElement("input");
+		submitButton.type = "submit";
+		submitButton.value = "alert input";
+
+
+		form.appendChild(input);
+		form.appendChild(submitButton);
+
+		form.addEventListener("submit", function () {
+			let userInput = input.value;
+			alert(userInput);
+		});
+		document.body.appendChild(form);
+	});
+
+	document.body.appendChild(button);
+}
